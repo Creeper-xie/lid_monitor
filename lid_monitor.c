eb77e6e7dbcb1e032050134f43b3418f1242b555
fanmo-yun <xiaomojun9527@outlook.com>
2024-07-03 19:57:45 +0800

    fix: prevent cannot receive any event

--- lid_monitor.c
+++ lid_monitor.c
@@ -1,59 +1,61 @@
-#include <assert.h>
-#include <errno.h>
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-
-#include <libudev.h>
 #include <libinput.h>
-
-#define PROJECT_NAME "lid_monitor"
+#include <fcntl.h>
+#include <unistd.h>
+#include <libudev.h>
+#include <sys/select.h>
 
 static int open_restricted(const char *path, int flags, void *user_data) {
     int fd = open(path, flags);
-    return fd < 0 ? -errno : fd;
+    if (fd < 0) {
+        perror("Failed to open device");
+    }
+    return fd;
 }
 
 static void close_restricted(int fd, void *user_data) {
     close(fd);
 }
 
-static const struct libinput_interface interface = {
+static const struct libinput_interface libinput_interface = {
     .open_restricted = open_restricted,
     .close_restricted = close_restricted,
 };
 
+static void handle_pointer_motion(struct libinput_event_pointer *pointer_event) {
+    double dx = libinput_event_pointer_get_dx(pointer_event);
+    double dy = libinput_event_pointer_get_dy(pointer_event);
+    printf("Pointer moved by %f, %f\n", dx, dy);
+}
+
 int main(void) {
-    struct libinput *li;
-    struct libinput_event *event;
-    //struct libinput_device *device;
-    struct udev *udev;
+    struct udev *udev = udev_new();
+    struct libinput *li = libinput_udev_create_context(&libinput_interface, NULL, udev);
 
-    udev = udev_new();
-    if (!udev) {
-        fprintf(stderr, "failed to initialize udev");
-        return 0;
-    }
-
-
-    li = libinput_udev_create_context(&interface, NULL, udev);
-
-    if (libinput_udev_assign_seat(li, "seat0") != 0) {
-        fprintf(stderr, "failed to assign seat");
-    }
+    libinput_udev_assign_seat(li, "seat0");
     
-    // li = libinput_path_create_context(&interface, NULL);
-    // device = libinput_path_add_device(li, "/dev/input/event1");
-    // libinput_device_ref(device);
-    libinput_dispatch(li);
-    while ((event = libinput_get_event(li)) != NULL) {
-        if (libinput_event_get_type(event) == LIBINPUT_EVENT_SWITCH_TOGGLE) {
-            
+    int fd = libinput_get_fd(li);
+    while (1) {
+        fd_set fds;
+        FD_ZERO(&fds);
+        FD_SET(fd, &fds);
+
+        if (select(fd + 1, &fds, NULL, NULL, NULL) > 0) {
+            libinput_dispatch(li);
+            struct libinput_event *event;
+            while ((event = libinput_get_event(li)) != NULL) {
+                if (libinput_event_get_type(event) == LIBINPUT_EVENT_POINTER_MOTION) {
+                    struct libinput_event_pointer *pointer_event = libinput_event_get_pointer_event(event);
+                    handle_pointer_motion(pointer_event);
+                }
+                libinput_event_destroy(event);
+            }
         }
-        libinput_event_destroy(event);
-        libinput_dispatch(li);
     }
+
     libinput_unref(li);
+    udev_unref(udev);
+
     return 0;
 }
