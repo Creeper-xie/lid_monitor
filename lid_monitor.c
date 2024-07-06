437060812f2b47df1d224b53bf701aeb5b9fb711
creeperxie <mingyue@ixcm.onmicrosoft.com>
2024-07-06 18:23:49 +0800

    chore: license by GPL-3.0-or-later

--- lid_monitor.c
+++ lid_monitor.c
@@ -1,3 +1,19 @@
+// lid_monitor -- a program to record state changes of your lid
+// Copyright (C) 2024  Sniventals
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #include <argp.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,7 +34,7 @@ const char* argp_program_bug_address = "<mingfengpigeon@gmail.com>";
 static char doc[] = "lib_monitor -- a program to record you lid";
 static struct argp argp = {0, 0, NULL, doc};
 
-// Connect to database with given path and execute given sql. Create database
+// Connect to database with given path and execute given SQL. Create database
 // if not exists. Open and close connection every times. When connection or
 // execution fails, close connection and return 1.
 static int sqlite_execute(char *path, char *sql) {
